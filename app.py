bba871bcd4e865eb35f83b5bdc9df63eb4d48e45
srafi1 <shakilrafi0@gmail.com>
2017-10-02 20:38:11 -0400

    done

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/response')
+def resp():
+    username = request.args.get('username')
+    method = request.method
+    return render_template('response.html',
+            username=username,
+            method=method)
 
 if __name__ == '__main__':
     app.run(debug=True);
